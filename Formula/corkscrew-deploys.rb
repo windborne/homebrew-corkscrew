0ba67af2fbd1f4f3407b5c6253f0c531b78ca67a
Kai Marshland <kaimarshland@gmail.com>
2025-11-04 21:20:59 -0800

    Update corkscrew formula to v1.2.3

--- Formula/corkscrew-deploys.rb
+++ Formula/corkscrew-deploys.rb
@@ -1,5 +1,5 @@
 
-VERSION = '1.2.2'
+VERSION = '1.2.3'
 
 class CorkscrewDeploys < Formula
   desc "Deploy and run code on another machine"
@@ -8,10 +8,10 @@ class CorkscrewDeploys < Formula
 
   if Hardware::CPU.intel?
     url "https://wb-data-public.s3.us-west-2.amazonaws.com/corkscrew/corkscrew-#{VERSION}-osx-x86_64.tar.gz"
-    sha256 "a1a4b6bd8e5b6594e0ae6235a4140aaa22543f0d3a0e6dc033fd672c6a4aac5f"
+    sha256 "f1e5903dcaa3f68fed55e019f115a37142092299d7d9630228eca9efe1d8f1bd"
   else
     url "https://wb-data-public.s3.us-west-2.amazonaws.com/corkscrew/corkscrew-#{VERSION}-osx-arm64.tar.gz"
-    sha256 "4dca3c1d894d51191c00660f1b13d8bdc8dd23329cf8fa4d3750a50cdf7c4695"
+    sha256 "e2d6cabf5f131c2273376687267a74d80c1cc4ce6261a0e7116a217d222fc9e1"
   end
 
   def install
